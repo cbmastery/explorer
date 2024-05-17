36950e9fd03f4897e9ff3abfa230d3f920f0764e
vassil <nomad@airone.attlocal.net>
2024-05-16 21:49:01 -0400

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import './app.css';
+import { useState } from 'react';
+import './App.css';
+import { Users } from './users';
 
 function App() {
+  const [query, setQuery] = useState("");
+
+  console.log(query);
+
   return (
-    <div className="app">
-      
+    <div className="App">
+      <input type="text" placeholder="Search..." className="search" onChange = {(e) => setQuery(e.target.value)} />
+      <ul className="list">
+        {Users.filter((user) => user.first_name.toLowerCase().includes(query)).map((user) => (
+          <li key={user.id} className="list-items">{user.first_name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
